b5e84d8d578f119db348c26c35c989e162a57e2f
Guney Unay <guneymarketing17@hotmail.com>
2025-10-01 22:35:34 +0300

    feat: add local font configuration for Avenir and Montserrat with various styles and weights

---
+++ apps/web/src/lib/font.ts
@@ -0,0 +1,155 @@
+import localFont from 'next/font/local'
+
+export const avenir = localFont({
+  src: [
+    {
+      path: '../../public/Fonts/avenir/Avenir-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-LightOblique.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-Book.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-BookOblique.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-MediumOblique.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-Heavy.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-HeavyOblique.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/avenir/Avenir-BlackOblique.ttf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-avenir',
+  display: 'swap',
+})
+
+export const montserrat = localFont({
+  src: [
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Thin.otf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-ThinItalic.otf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-ExtraLight.otf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-ExtraLightItalic.otf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-SemiBold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-SemiBoldItalic.otf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-ExtraBold.otf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-ExtraBoldItalic.otf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/Fonts/montserat/Montserrat-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-montserrat',
+  display: 'swap',
+})
--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Converts popularity score to 1-5 rating
+ * @param popularityScore
+ * @returns
+ */
+export const getRating = (popularityScore: number) => {
+  return Math.min(5, Math.max(1, Math.round(popularityScore * 2)))
+}
